eb5c555474a26522bc29ef184b9ff22ff59a2b3d
Mr.B <itjkiss@gmail.com>
2022-03-28 18:27:48 +0800

    ongo

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -7,6 +7,6 @@
 
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
+export async function handler(req, res) {
     res.status(200).json({ name: 'John Doe' })
 }
